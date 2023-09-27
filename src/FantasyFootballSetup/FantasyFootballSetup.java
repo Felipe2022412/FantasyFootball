d226669a28e33a7a7a9a09204746d5e8ab45cbce
Felipe2022412 <2022412@student.cct.ie>
2023-09-27 14:23:19 +0100

    changed package name
    changed package name

--- src/FantasyFootballSetup/FantasyFootballSetup.java
+++ src/FantasyFootballSetup/FantasyFootballSetup.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package samclass;
+package FantasyFootballSetup;
 
 import java.io.File;
 import java.io.FileNotFoundException;
